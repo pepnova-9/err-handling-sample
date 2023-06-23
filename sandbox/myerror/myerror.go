65ee6027652e2bd1e18e3f6135862bd06eef79f1
horai <ptn-shotaro.horai@zozo.com>
2023-06-23 18:17:49 +0900

    fix

--- sandbox/myerror/myerror.go
+++ sandbox/myerror/myerror.go
@@ -29,19 +29,19 @@ func (e *MyError) Error() string {
 	return fmt.Sprintf("[%d] %s", e.Code, e.Message)
 }
 
-//func (e *MyError) Is(target error) bool {
-//	if t, ok := target.(*MyError); ok {
-//		return e.Code == t.Code
-//	}
-//	return false
-//}
+func (e *MyError) Is(target error) bool {
+	if t, ok := target.(*MyError); ok {
+		return e.Code == t.Code
+	}
+	return false
+}
 
-//func (e *MyError) Unwrap() error { // ★ errors.Unwrap()に関係するメソッド（後述）
-//	return e.childErr
-//}
+func (e *MyError) Unwrap() error { // ★ errors.Unwrap()に関係するメソッド（後述）
+	return e.childErr
+}
 
 func ErrorHandling1() {
-	fmt.Println("=== ErrorHandling1 ===")
+	fmt.Println("=== ErrorHandling1 IsとAsの使い方 ===")
 	err := raise101Error()
 	fmt.Printf("%v \n", err)
 
@@ -74,12 +74,14 @@ func ErrorHandling1() {
 		fmt.Printf("Code: %v \n", myError.Code)
 		fmt.Printf("Message: %v \n", myError.Message)
 	}
+
+	fmt.Println("")
 }
 
 var OtherPackageErr = errors.New("other package error")
 
 func ErrorHandling2() {
-	fmt.Println("=== ErrorHandling2 ===")
+	fmt.Println("=== ErrorHandling2 Unwrapの使い方 ===")
 	myError := NewMyError(202, "my error having child error", OtherPackageErr)
 
 	if errors.Is(myError, OtherPackageErr) {
@@ -87,9 +89,36 @@ func ErrorHandling2() {
 	} else {
 		fmt.Printf("OtherPackageErrが含まれていない \n")
 	}
+	fmt.Println("")
+}
+
+func ErrorHandling3() {
+	fmt.Println("=== ErrorHandling3 errors.Joinの使い方")
+
+	joinedErr := errors.Join(Err101Error, OtherPackageErr)
+	if errors.Is(joinedErr, OtherPackageErr) {
+		fmt.Printf("OtherPackageErrを拾える: %v \n", OtherPackageErr)
+	}
+	fmt.Printf("Joinしたエラーを出力するとこうなる(最初の要素のmsgしか出力されない): %v \n", joinedErr)
+
+	joinedErr2 := fmt.Errorf("ここにメッセージを入れてJoinもできる: %w: %w \n", Err101Error, OtherPackageErr)
+	if errors.Is(joinedErr2, OtherPackageErr) {
+		fmt.Printf("ErrorfでもJoinと一緒: %v \n", OtherPackageErr)
+	}
+	fmt.Printf("ErrorfしてJoinしたエラーを出力するとこうなる(複数のエラーのmsgを出力できる): %v \n", joinedErr2)
+
+	myError := &MyError{}
+	if ok := errors.As(joinedErr2, &myError); ok {
+		fmt.Println("Joinしたエラーもerrors.Asで取り出せる")
+		fmt.Printf("Code: %v \n", myError.Code)
+		fmt.Printf("Message: %v \n", myError.Message)
+	}
+
+	fmt.Println("")
 }
 
 func main() {
-	ErrorHandling1()
-	ErrorHandling2()
+	//ErrorHandling1()
+	//ErrorHandling2()
+	ErrorHandling3()
 }
